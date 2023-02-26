818792097a94d3845229d32e10782be8224980b8
CHE <worasornjunpuan@gmail.com>
2023-02-26 09:06:56 +0700

    create 2 test file
    test_input-format.py
    finish new input (hour,minute) and the anti error for input
    
    try_new-feature.py
    develop version of new input
    and finish of convert output time

---
+++ Test_input-format.py
@@ -0,0 +1,37 @@
+import time
+
+
+def covert_input():
+    input_error_check = True
+    while input_error_check == True:
+
+        input_lenght = str(input("Input how long you want to AFK (Hour,Minute)"))
+
+        list_lenght = input_lenght.split(",")
+
+        try:
+            hour_lenght = float(list_lenght[0])
+            min_lenght = float(list_lenght[1])
+            input_error_check = False
+        except:
+            print()
+            print("there an error")
+            print("make sure to input only the number")
+            print("and with Hour,Minute format")
+            print("for example 1,30 ")
+            print()
+
+    global apk_lenght
+    apk_lenght = ((hour_lenght * 3600) + (min_lenght * 60)) / 1.43
+    print(apk_lenght)
+    for i in range(int(apk_lenght)):
+        print("hello", i)
+    print(apk_lenght)
+
+
+covert_input()
+
+
+for i in range(int(apk_lenght)):
+    print(i)
+print(apk_lenght)
